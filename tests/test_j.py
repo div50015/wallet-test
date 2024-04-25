8881757a842cb1191140d805d39ba025311688ab
Igor Degtyarenko <div50015@mail.ru>
2024-04-25 22:14:41 +0300

    edit 25.04

--- tests/test_j.py
+++ tests/test_j.py
@@ -1,22 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
-
+import time
 
 
 
 def test_js():
     driver = webdriver.Chrome()
     driver.get("https://koshelek.ru/authorization/signup")
-
+    time.sleep(10)
     #   driver.get("file:///localhost")
 
     # shadow_host = driver.find_element(By.TAG_NAME, "section").find_element(By.CLASS_NAME, "remoteComponent")
     # shadow_root = shadow_host.shadow_root
     # shadow_content = shadow_root.find_element(By.TAG_NAME, 'input')
 
-    shadow_host = driver.find_element(By.TAG_NAME, 'section' )
-    # shadow_root = shadow_host.shadow_root
-    # shadow_content = shadow_root.find_element(By.TAG_NAME, "input")
+    shadow_host = driver.find_element(By.CSS_SELECTOR, ".remoteComponent")
+    shadow_root = shadow_host.shadow_root.
+    shadow_content = shadow_root.find_element(By.CSS_SELECTOR, 'input')
 
     driver.quit()
+
+
--- tests/test_t.py
+++ tests/test_t.py
@@ -3,7 +3,9 @@ from selene import browser, have
 import os
 from selene import command
 from selenium.webdriver.common.by import By
-
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 def test_complete_todo():
     browser.open('/')
     # уменьшение размера изображения в 0.8 раза
@@ -15,7 +17,7 @@ def test_complete_todo():
     # browser.all('.v-btn.v-btn--router.v-btn--text.v-btn--tile.theme--light.v-size--default')[3].click()
     # browser.all('.v-text-field__slot')[1]
     # browser.all('input')[1].locate().shadow_root.find_element()
-    browser.element('.remoteComponent').locate()
+    browser.element('.remoteComponent').c
 
     time.sleep(10)
     browser.close()
