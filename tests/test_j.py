6090bc4d65fc7d336e3b1bf7fdfec27dd2a0374f
Igor Degtyarenko <div50015@mail.ru>
2024-04-26 10:07:12 +0300

    Merge remote-tracking branch 'origin/master'

--- tests/test_j.py
+++ tests/test_j.py
@@ -8,16 +8,34 @@ import time
 def test_js():
     driver = webdriver.Chrome()
     driver.get("https://koshelek.ru/authorization/signup")
-    time.sleep(10)
-    #   driver.get("file:///localhost")
+    time.sleep(5)
 
-    # shadow_host = driver.find_element(By.TAG_NAME, "section").find_element(By.CLASS_NAME, "remoteComponent")
-    # shadow_root = shadow_host.shadow_root
-    # shadow_content = shadow_root.find_element(By.TAG_NAME, 'input')
-
-    shadow_host = driver.find_element(By.CSS_SELECTOR, ".remoteComponent")
-    shadow_root = shadow_host.shadow_root.
+    # error cod
+    shadow_host = driver.find_element(By.TAG_NAME, "section").find_element(By.CLASS_NAME, "remoteComponent")
+    shadow_root = shadow_host.shadow_root
     shadow_content = shadow_root.find_element(By.CSS_SELECTOR, 'input')
+    print(f'\r\ntype host = {type(shadow_host)}')
+    print(f'type root = {type(shadow_root)}')
+    print(f'type cont= {type(shadow_content)}')
+
+
+    # work cod
+    # shadow_host = driver.find_element(By.CSS_SELECTOR, ".remoteComponent")
+    # shadow_root = shadow_host.shadow_root
+    # shadow_content = shadow_root.find_element(By.CSS_SELECTOR, 'input')
+
+    # js script
+    # shadow_host = driver.find_element(By.CSS_SELECTOR, '.remoteComponent')
+    # print(F'\r\ntype = {type(shadow_host)}')
+    # print(F'\r\nlist = {shadow_host}')
+    # children = driver.execute_script('return arguments[0].shadowRoot.children', shadow_host)
+    # print(F'\r\ntype = {type(children)}')
+    # print(F'\r\nlist = {children}')
+    # # shadow_content = next(child for child in children if child.get_attribute('id') == 'input')
+    # shadow_content = children[0]
+    # print(F'\r\ntype = {type(shadow_content)}')
+    # print(F'\r\nlist = {shadow_content}')
+    # shadow_content.find_element(By.TAG_NAME, 'input')
 
     driver.quit()
 
