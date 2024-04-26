6316455a15eb012b8307f5952d119dcdd71155dd
Igor Degtyarenko <div50015@mail.ru>
2024-04-26 15:58:11 +0300

    Merge remote-tracking branch 'origin/master'

--- tests/test_j.py
+++ tests/test_j.py
@@ -1,8 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
 import time
-
+from selenium.webdriver.common.keys import Keys
 
 
 def test_js():
@@ -13,11 +12,22 @@ def test_js():
     # error cod
     shadow_host = driver.find_element(By.TAG_NAME, "section").find_element(By.CLASS_NAME, "remoteComponent")
     shadow_root = shadow_host.shadow_root
-    shadow_content = shadow_root.find_element(By.CSS_SELECTOR, 'input')
-    print(f'\r\ntype host = {type(shadow_host)}')
-    print(f'type root = {type(shadow_root)}')
-    print(f'type cont = {type(shadow_content)}')
+    shadow_content1 = shadow_root.find_element(By.CSS_SELECTOR, 'input').send_keys('1')
+    shadow_content2 = shadow_root.find_elements(By.CSS_SELECTOR, 'input')[1].click()
 
+    shadow_host1 = driver.find_element(By.TAG_NAME, "section").find_element(By.CLASS_NAME, "remoteComponent")
+    shadow_root1 = shadow_host.shadow_root
+    shadow_content11 = shadow_root.find_elements(By.CSS_SELECTOR, 'span')[21].text()
+    print(f'\r\ntype cont11 = {shadow_content11}')
+
+    # shadow_text1 = shadow_root.find_element(By.XPATH(".//span[contains(text(),' Допустимые символы (от 6 до 32): a-z, 0-9, _. Имя должно начинаться с буквы ')]"))
+
+    time.sleep(5)
+    # print(f'\r\ntype host = {type(shadow_host)}')
+    # print(f'type root = {type(shadow_root)}')
+    # print(f'type cont = {type(shadow_content1)}')
+    # print(f'cont = {shadow_content1}')
+    driver.quit()
 
     # work cod
     # shadow_host = driver.find_element(By.CSS_SELECTOR, ".remoteComponent")
