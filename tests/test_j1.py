c0c029a65c259284c501bb370b457afb91d2c261
Igor Degtyarenko <div50015@mail.ru>
2024-04-28 09:50:20 +0300

    edit 28.04

--- tests/test_j1.py
+++ tests/test_j1.py
@@ -3,7 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-@pytest.mark.parametrize("name", [('username')])
+
+@pytest.mark.parametrize("name", [('username'), ('username'), ('username'), ('username'), ('username'), ('username'),
+                                  ('username'), ('username'), ('username')])
 def test_bad_name(name):
     driver = webdriver.Chrome()
     driver.get("https://koshelek.ru/authorization/signup")
@@ -18,13 +20,19 @@ def test_bad_name(name):
 
     shadow_content_input_name = shadow_content_div_name.find_element(By.CSS_SELECTOR, "input").send_keys(f'{name}')
     shadow_content_input_email = shadow_content_div_email.find_element(By.CSS_SELECTOR, "input").click()
+    time.sleep(1)
 
-    shadow_content_div_name = shadow_content_form.find_element(By.XPATH, ".//div[@data-wi='user-name']")
-    shadow_content_text = shadow_content_div_name.find_elements(By.CSS_SELECTOR, "span")
-    print(shadow_content_text[0].text)
-    print(shadow_content_text[1].text)
-    print(shadow_content_text[2].text)
-    print(shadow_content_text[3].text)
+    shadow_content_text = shadow_content_div_name.find_element(By.XPATH, ".//span[contains(text(), 'Имя пользователя уже занято')]")
+
+    assert shadow_content_text.text == 'Имя пользователя уже занято'
+
+    # print(f'\r\ncont = {shadow_content_text.text}')
+    # i = 0
+    # print('\r\n')
+    # for v in shadow_content_text:
+    #     print(f'i{i} = {v.text}')
+    #     i+=1
+
     # shadow_host1 = driver.find_element(By.CLASS_NAME, "remoteComponent")
     # shadow_root1 = shadow_host1.shadow_root
     # shadow_content3 = shadow_root.find_element(By.CSS_SELECTOR, 'form')
@@ -35,7 +43,5 @@ def test_bad_name(name):
     #     i = i + 1
     #     print(f'cont{i} = {v.text}')
 
-    # assert shadow_content4[0].text == 'Имя пользователя уже занято'
 
-    time.sleep(5)
     driver.quit()
