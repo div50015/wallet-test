9cec7bc4d7187f3a11009fcd032909967efbe4ce
Igor Degtyarenko <div50015@mail.ru>
2024-05-03 09:06:16 +0300

    Edit 3.04 9-00

--- tests/tests_field_not_filled.py
+++ tests/tests_field_not_filled.py
@@ -13,6 +13,5 @@ def test_input_null_name(web_browser):
         assert app.shadow_span_name().text == 'Поле не заполнено'
         assert app.shadow_span_email().text == 'Поле не заполнено'
         assert app.shadow_span_password().text == 'Поле не заполнено'
-        assert app.shadow_span_password().text == 'Поле не заполнено'
         assert app.shadow_span_checkbox().get_attribute(
             'class') == 'v-input v-input--has-state v-input--hide-details v-input--dense theme--light v-input--selection-controls v-input--checkbox error--text'
